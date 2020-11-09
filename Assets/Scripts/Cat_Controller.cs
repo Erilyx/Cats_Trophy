5093b3110ad1c39add19ed563e6972e4df92f5eb
Erilyx <erilyx@gmail.com>
2020-11-08 19:54:16 -0500

    Prototype
    First commit ... has some custom sprites cat cats

---
+++ Assets/Scripts/Cat_Controller.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat_Controller : MonoBehaviour
+{
+
+    private float startPosX, startPosY;
+    private bool isBeingHeld = false;
+    public Rigidbody2D rb;
+    public PolygonCollider2D polyCol;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        polyCol = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeingHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+
+            rb.freezeRotation = true;  // so cats don't spin out of control when moving
+           // polyCol.enabled = false;
+
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - transform.localPosition.x;
+            startPosY = mousePos.y - transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+        
+    }
+
+
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+        rb.freezeRotation = false;
+       // polyCol.enabled = true;   //why does it act differently some times?
+
+
+    }
+}
---
+++ Assets/YellowLine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowLine : MonoBehaviour
+{
+
+    public float lineHeight = 0;
+    private float lineRiser = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cat"))
+        {
+            //lineRiser += 0.01f;
+            transform.position = new Vector3(0, collision.transform.position.y + lineRiser, 0);
+        }
+    }
+
+}
